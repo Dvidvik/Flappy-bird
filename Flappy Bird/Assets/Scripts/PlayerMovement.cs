a5aa702088132e58cc59e52126bb68b65db25c99
Jan Karban <jan.karban2005@gmail.com>
2023-02-09 22:26:19 +0100

    Changing jump mechanic

--- Flappy Bird/Assets/Scripts/PlayerMovement.cs
+++ Flappy Bird/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public int jumpHight;
+    public float jumpHight;
+    public float fall;
     public Rigidbody2D body;
 
     private void Update()
     {
+        float positionY = transform.position.y;
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            body.AddForce(Vector2.up * jumpHight);
+            body.MovePosition(new Vector2(0, positionY + jumpHight));
             print("Space was pressed");
         }
+        body.MovePosition(new Vector2(0, positionY - fall));
+
     }
 }
